36d4c168acf19f88045e163edcc5b1a59ac67c34
wangxiaowei <wangxiaoweilove@126.com>
2021-10-12 16:25:33 +0800

    asp.net core示例代码

---
+++ Sample02/WebApplication2/WebApplication2/Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)//使用默认的配置信息来初始化一个新的IWebHostBuilder实例
+            .ConfigureAppConfiguration((hostingContext, config) =>
+            {
+                var env = hostingContext.HostingEnvironment;
+
+                config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                      .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                      .AddJsonFile("Content.json", optional: false, reloadOnChange: false)
+                      .AddEnvironmentVariables();
+
+            })
+            .UseStartup<Startup>();// 为Web Host指定了Startup类
+    }
+}
